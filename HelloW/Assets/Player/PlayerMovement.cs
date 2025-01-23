cfb152ec42a5914c9b8ab1d21d3c87171d716f30
MA-Silent <38739@ma-web.nl>
2025-01-23 14:05:09 +0100

    Jump

---
+++ HelloW/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 4;
+    private Animator _animator;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    /*
+        Hier komt het volgende blok code
+    */
+    void Update()
+    {
+        Vector3 movement = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += transform.forward * movementSpeed * Time.deltaTime;
+            _animator.SetFloat("speed", 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement -= transform.forward * movementSpeed * Time.deltaTime;
+        }
+        transform.position += movement.normalized * movementSpeed * Time.deltaTime;
+    }
+}
--- HelloW/Assets/Scripts/Movement.cs
+++ HelloW/Assets/Scripts/Movement.cs
@@ -20,12 +20,16 @@ public class Movement : MonoBehaviour
         
 
         transform.position += transform.forward * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-
         transform.Rotate(0, rotSpeed* Input.GetAxisRaw("Horizontal") * Time.deltaTime, 0);
-        if(Input.GetButtonDown("Jump")&&isGrounded())
+        if(Input.GetButtonDown("Jump"))
         {
             Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(new Vector3(0, 100, 0)*JumpPower);
+            rb.AddForce(new Vector3(0, 210, 0)*JumpPower);
+            _animator.SetFloat("Jump", 1);
+        }
+        else
+        {
+            _animator.SetFloat("Jump", 0);
         }
         if (Input.GetAxisRaw("Vertical") != 0)
         {
@@ -43,10 +47,10 @@ public class Movement : MonoBehaviour
     bool isGrounded()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, GroundCheckDistance))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit,GroundCheckDistance))
         {
             return true;
         }
-       return false;
+        return false;
     }
 }
